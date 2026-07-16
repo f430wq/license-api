56dad330d06d90821cf02dc649d3359a7790db65
f430wq <renzyreally@gmail.com>
2026-07-16 23:09:49 +0200

    Update script.js

--- src/routes/script.js
+++ src/routes/script.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../database");
+const db = require("../database/database");
 
 
 // Script Roblox à retourner après validation
