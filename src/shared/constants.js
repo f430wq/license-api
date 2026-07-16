5a1a11f4c290825cc9bbecb64773a26d6ab63a7d
f430wq <renzyreally@gmail.com>
2026-07-16 22:08:06 +0200

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,1 +1,18 @@
+module.exports = {
+    LICENSE_STATUS: {
+        ACTIVE: "active",
+        USED: "used",
+        EXPIRED: "expired",
+        REVOKED: "revoked"
+    },
 
+    HWID_STATUS: {
+        LINKED: "linked",
+        RESET: "reset"
+    },
+
+    API_STATUS: {
+        ONLINE: "online",
+        OFFLINE: "offline"
+    }
+};
