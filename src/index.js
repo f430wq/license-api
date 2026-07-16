f0574e22843e6f305afed0229b483ecfbb47a141
f430wq <renzyreally@gmail.com>
2026-07-16 22:11:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ const cors = require("cors");
 require("dotenv").config();
 
 const { initDatabase } = require("./database/database");
+const licenseRoutes = require("./routes/licenses");
+
 
 const app = express();
 
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
 
-// Test API
+// Routes
+app.use("/licenses", licenseRoutes);
+
+
+// API Status
 app.get("/", (req, res) => {
     res.status(200).json({
         success: true,
@@ -21,10 +29,11 @@ app.get("/", (req, res) => {
 });
 
 
-// Initialisation database
+// Database
 initDatabase();
 
 
+// Start Server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
