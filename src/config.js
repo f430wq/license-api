27b4dfe6e25f7740581aac9412f58457ef10b483
f430wq <renzyreally@gmail.com>
2026-07-16 22:07:35 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,14 @@
+require("dotenv").config();
 
+module.exports = {
+    port: process.env.PORT || 3000,
+
+    database: {
+        file: process.env.DATABASE_FILE || "./database/license.sqlite"
+    },
+
+    license: {
+        keyLength: 32,
+        hwidResetCooldown: 86400000
+    }
+};
