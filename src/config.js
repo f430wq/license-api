1c9930cf898ebc3ab6e851b2d673b85e9bec4c9c
f430wq <renzyreally@gmail.com>
2026-07-17 20:01:53 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
+const path = require("path");
 require("dotenv").config();
 
 module.exports = {
     port: process.env.PORT || 3000,
 
     database: {
-        file: process.env.DATABASE_FILE || "./database/license.sqlite"
+        file: process.env.DATABASE_FILE || path.join(__dirname, "database", "license.sqlite")
     },
 
     license: {
